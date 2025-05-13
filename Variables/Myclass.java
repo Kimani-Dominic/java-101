67b87417a29ae0699c9a1ab5dd532b366bbf5090
Kimani-Dominic <muiruridominic001@gmail.com>
2025-05-13 13:08:19 +0300

    variables & math operations

---
+++ Variables/Myclass.java
@@ -0,0 +1,26 @@
+package Variables;
+
+public class Myclass {
+    public static void main(String[] args) {
+        //Variables
+        var myVar = "Day 2 going on well ";
+        //strings
+        String statement = "learning Java variables & data types";
+        //Integers
+        int No = 123;
+        //bytes
+        byte myByte = 12;
+        // floating integers
+        double myFloat = 89.99;
+
+        double simpleCalculation = myFloat * myByte;
+
+        System.out.print(myVar);
+        System.out.println(statement);
+        System.out.println(No);
+        System.out.println(myByte);
+        System.out.println(myFloat);
+        System.out.printf("Multiplication of 12 & 89.99", simpleCalculation);
+
+    }
+}
--- classes/Classes.java
+++ classes/Classes.java
@@ -1,3 +1,5 @@
+package classes;
+
 public class Classes {
     public static void main(String[] args) {
         System.out.print(FirstMessage.firstMessage());
